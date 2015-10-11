65b9c0a46c1606d69bd6e4f14876ddd8e9676f12
koseki <dainaso97@gmailcom>
2015-10-11 12:07:48 +0900

    first commit

---
+++ UIWebViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIWebViewSample
+//
+//  Created by 前田 晃良 on 2015/10/11.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
